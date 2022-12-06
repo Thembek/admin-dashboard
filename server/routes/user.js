9ff8b135d7ef047125b1f1840d0a47c300304123
Andile <andile.goba@younglings.africa>
2022-12-06 17:40:33 +0200

    transaction route update

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,11 +6,72 @@ import getCounrtyIso3 from 'country-iso-2-to-3';
 import express from 'express';
 const router = express.Router();
 
-router.get('/products', );
+router.get('/products', async (req, res) => {
+    try {
+        const products = await Product.find();
 
-router.get('/customers', );
+        const productWithStats = await Promise.all(
+            products.map(async (product) => {
+                const stat = await ProductStat.find({
+                    productId: product._id,
+                });
+                return {
+                    ...product.doc,
+                    stat,
+                };
+            })
+        );
 
-router.get('/transactions',)
+        res.status(200).json(productWithStats);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+});
+
+router.get('/customers', async (req, res) => {
+    try {
+        const customers = await User.find({ role: 'user' }).select("-password");
+        res.status(200).json(customers);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+});
+
+router.get('/transactions', async (req, res) => {
+    try{
+        const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
+
+        const generateSort = () => {
+            const sortParsed = JSON.parse(sort);
+            const sortFormatted = {
+                [sortParsed.field]: (sortParsed.sort = "asc" ? 1 : -1),
+            };
+            return sortFormatted;
+        };
+        const sortFormatted = Boolean(sort) ? generateSort() : {};
+
+        const transactions = await Transaction.find({
+            $or: [
+                { cost: { $regex: new RegExp(search, "i") } },
+                { userId: { $regex: new RegExp(search, "i") } },
+            ],
+        })
+         .sort(sortFormatted)
+         .skip(page * pageSize)
+         .limit(pageSize)
+
+        const total = await Transaction.countDocuments({
+            name: { $regex: search, $options: "i" },
+        });
+
+        res.status(200).json({
+            transactions,
+            total,
+        })
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+})
 
 router.get('/geography', );
 

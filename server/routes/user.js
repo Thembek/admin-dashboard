906aaa8f1060f1f141f7136dc6af3a40a7c5e631
Andile <andile.goba@younglings.africa>
2022-12-06 17:25:17 +0200

    user router update

---
+++ server/routes/user.js
@@ -0,0 +1,17 @@
+import User from '../model/User.js';
+import Product from '../model/Product.js';
+import Transaction from '../model/Transaction.js';
+import ProductStat from '../model/ProductStat.js';
+import getCounrtyIso3 from 'country-iso-2-to-3';
+import express from 'express';
+const router = express.Router();
+
+router.get('/products', );
+
+router.get('/customers', );
+
+router.get('/transactions',)
+
+router.get('/geography', );
+
+export default router;

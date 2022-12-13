da3012e1caaa3025ca902cec3664b8c3ed82da1d
Andile <andile.goba@younglings.africa>
2022-12-13 16:03:47 +0200

    server file errors update commit

--- server/routes/sales.js
+++ server/routes/sales.js
@@ -1,14 +1,8 @@
-import OverallStat from '../model/OverallStat.js';
-import express from 'express';
+import express from "express";
+import { getSales } from "../controllers/sales.js";
+
 const router = express.Router();
 
-router.get('/sales', async (req, res) => {
-    try{
-        const overallStats = await OverallStat.find();
-        res.status(200).json(overallStats[0]);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-});
+router.get("/sales", getSales);
 
 export default router;

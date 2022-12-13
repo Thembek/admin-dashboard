8276a0f26c56accffa9e789a25af79723546f214
Andile <andile.goba@younglings.africa>
2022-12-13 13:50:24 +0200

    Monthly file commit

--- client/src/screens/layout/index.jsx
+++ client/src/screens/layout/index.jsx
@@ -5,4 +5,34 @@ import { Box, useMediaQuery } from '@mui/material';
 import { useGetUserQuery } from '../../state/api';
 
 import Navbar from '../../components/Navbar';
-import Sidebar from '../../components/'
+import Sidebar from '../../components/Sidebar';
+
+const Layout = () => {
+    const isNonMobile = useMediaQuery("(min-width: 600px)");
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const userId = useSelector((state) => state,gloabl.userId);
+    const { data } = useGetUserQuery(userId);
+
+    return(
+        <Box display={ isNonMobile ? "flex" : "block" } width="100%" height="100%">
+            <Sidebar 
+                user={data || {} }
+                isNonMobile={isNonMobile}
+                drawerWidth="250px"
+                isSidebarOpen={isSidebarOpen}
+                setIsSidebarOpen={setIsSidebarOpen}
+            />
+
+            <Box flexGrow={1}>
+                <Navbar 
+                    user={data || {} }
+                    isSidebarOpen={isSidebarOpen}
+                    setIsSidebarOpen={setIsSidebarOpen}
+                />
+                <Outlet />
+            </Box>
+        </Box>
+    );
+}
+
+export default Layout;
